f24dfa30f8d2ad77038bd47491617319ca00bc47
Nazg0r <gorchinshiy83@gmail.com>
2025-06-19 19:12:47 +0300

    feat: write commands mapping extensions

---
+++ src/Core/Hexagonal.Application/Mappings/CommandMappings.cs
@@ -0,0 +1,26 @@
+﻿using Hexagonal.Application.Commands;
+using Hexagonal.Domain.Entities;
+
+namespace Hexagonal.Application.Mappings
+{
+	public static class CommandMappings
+	{
+		public static User ToDomain(this CreateUserCommand command) =>
+			new User(
+				command.Email,
+				command.UserName,
+				command.Name,
+				command.Surname,
+				command.Birthday
+			);
+
+		public static User ToDomain(this UpdateUserCredentialsCommand command) =>
+			new User(
+				command.Email,
+				command.UserName,
+				command.Name,
+				command.Surname,
+				command.Birthday
+			);
+	}
+}
