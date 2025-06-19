b28d75b953ca0fa246d075ee0b3433375d863bba
Nazg0r <gorchinshiy83@gmail.com>
2025-06-19 23:38:05 +0300

    feat: create application startup project

---
+++ src/ApplicationSetup/Program.cs
@@ -0,0 +1,13 @@
+﻿using Hexagonal.Adapter.Auth;
+using Hexagonal.Adapter.DetailedNotification;
+using Hexagonal.Adapter.InMemory;
+using Hexagonal.Adapter.Rest;
+
+var api = new RestAdapter(args, options =>
+{
+	options.AddInMemoryAdapter();
+	options.AddAuthAdapter();
+	options.AddDetailedNotificationAdapter();
+});
+
+await api.StartAsync();
